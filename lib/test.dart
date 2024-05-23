6834b926bec90a2bc48110312a460defee729677
kecaseo9 <kecaseo9@gmail.com>
2024-05-23 17:39:04 +0900

    되어라.

---
+++ lib/test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class test1 extends StatefulWidget {
+  const test1({super.key});
+
+  @override
+  State<test1> createState() => _test1State();
+}
+
+class _test1State extends State<test1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
